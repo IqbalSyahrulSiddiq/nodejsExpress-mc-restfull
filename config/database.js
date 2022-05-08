776b801746aceba9372d774b2f7069e8d1617eef
Iqbal Syahrul Siddiq <ikbalmuhammad53@gmail.com>
2022-05-08 22:49:28 +0700

    remove credential

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 //koneksi database
 const koneksi = mysql.createConnection({
-    host: 'kenalkoding.com',
-    user: 'u1685809_iqbalsyahrulsiddiq',
-    password:'Persibbandung1933',
-    database: 'u1685809_nodejs_restfull',
+    host: 'xxxx',
+    user: 'xxxx',
+    password:'xxxx',
+    database: 'xxxx',
     multipleStatements: true
 });
 
